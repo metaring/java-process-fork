f03d7e2bab3eaecc12cfcaa23a66699eeeffaa4c
hhovhann <haik.hovhanisyan@gmail.com>
2019-11-25 20:40:23 +0400

    Upgrade plugin version. Upgrade java version to 11. Modified getMyPid functionality

--- src/main/java/com/metaring/java/process/fork/PidRetriever.java
+++ src/main/java/com/metaring/java/process/fork/PidRetriever.java
@@ -1,12 +1,12 @@
 /**
  *    Copyright 2019 MetaRing s.r.l.
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,9 +16,7 @@
 
 package com.metaring.java.process.fork;
 
-import java.lang.management.RuntimeMXBean;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.Kernel32;
@@ -28,13 +26,8 @@ public class PidRetriever {
 
     @SuppressWarnings("restriction")
     public static final long getMyPid() throws Exception {
-        RuntimeMXBean runtime = java.lang.management.ManagementFactory.getRuntimeMXBean();
-        Field jvm = runtime.getClass().getDeclaredField("jvm");
-        jvm.setAccessible(true);
-        sun.management.VMManagement mgmt = (sun.management.VMManagement) jvm.get(runtime);
-        Method pid_method = mgmt.getClass().getDeclaredMethod("getProcessId");
-        pid_method.setAccessible(true);
-        return Long.parseLong(pid_method.invoke(mgmt).toString());
+        // todo check is this the equivalent of what have before https://dzone.com/articles/java-9-besides-modules
+        return ProcessHandle.current().pid();
     }
 
     public static final PidRetriever getProcessPid(Process process) {
